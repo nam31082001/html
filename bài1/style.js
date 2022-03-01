092b016259e5b7abcfe53f266da713fc85c2c472
nam31082001 <caovannam785@gmail.com>
2022-03-01 16:17:38 +0700

    bài1

---
+++ bài1/style.js
@@ -0,0 +1,6 @@
+alert('xin chào')
+
+
+let yourName = prompt("Hãy nhập tên của bạn");
+
+document.write("Xin chào " + yourName);
---
+++ js/New folder/style.js
@@ -0,0 +1,83 @@
+// alert('xin chào')
+// let yourName = prompt("Hãy nhập tên của bạn");
+// document.write("Xin chào " + yourName);
+// let i = 1;
+// let f = 20.5;
+// let b = true;
+// let s = 'Hà Nội';
+
+// document.write('i = ' + i);
+// document.write('<br/>');
+// document.write('f = ' + f);
+// document.write('<br/>');
+// document.write('b = ' + b);
+// document.write('<br/>');
+// document.write('s = ' + s);
+// document.write('<br/>');
+// document.write('i=' + i);
+// document.write('<br/>');
+
+// let width = 20;
+// let height = 10;
+// let area = width * height;
+// document.write('Area = ' + area);
+// document.write('<br/>');
+
+
+
+
+// let chieudai;
+// let chieurong;
+//  chieudai = prompt("nhap vào chieu dai");
+//  chieurong = prompt("nhap vao chieu rong");
+// let dai =parseInt(chieudai);
+// let rong =parseInt(chieurong);
+// let dientich = dai * rong;
+// document.write("diện tích là: " + dientich);
+
+
+
+
+// let a=prompt('nhap năm');
+// let b=prompt('nhap ngay');
+// let c=prompt('nhap tháng');
+// let d=a+"-"+b+"-"+c;
+// document.write("Kết quả hiển thị trên màn hình là: " + d)
+
+// let bankinh=prompt('bán kính hình tròn là :');
+// let r=3.14;
+// let c=2*bankinh*r;
+// let s=bankinh*bankinh*r;
+// document.write("diện tích là: " + s);
+// document.write('<br/>');
+// document.write("chu vi là: " + c);
+
+
+
+// let a=prompt('nhap so tien gửi vào ngân hàng');
+// let b=prompt('nhap so lai suât trên 1 nam');
+// let c=prompt('nhap so nam gửi');
+// let e=a*(b/100);
+// let d =a+c*e;
+// document.write("Sau c năm số tiền cả gốc lẫn lãi là " + d);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
